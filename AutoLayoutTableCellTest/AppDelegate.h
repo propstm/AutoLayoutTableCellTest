92cf9b86367a18e88c2592a7bd24b63639b689b0
propstm <propstm@gmail.com>
2016-11-16 08:23:11 -0500

    Initial Commit

---
+++ AutoLayoutTableCellTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutTableCellTest
+//
+//  Created by Matt Propst on 11/16/16.
+//  Copyright © 2016 Matt Propst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayoutTableCellTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutTableCellTest
+//
+//  Created by Matt Propst on 11/16/16.
+//  Copyright © 2016 Matt Propst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
